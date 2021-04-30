53e0c22eb65aa813c8ceffe269b0b8db983e5888
yangwanhong <yangwanhong@linklogis.com>
2021-04-30 16:57:59 +0800

    ddelete

--- target/classes/static/js/blog.js
+++
@@ -1,5 +0,0 @@
-function changeUrlArg(url, arg, val){
-    var pattern = arg+'=([^&]*)';
-    var replaceText = arg+'='+val;
-    return url.match(pattern) ? url.replace(eval('/('+ arg+'=)([^&]*)/gi'), replaceText) : (url.match('[\?]') ? url+'&'+replaceText : url+'?'+replaceText);
-}
